07d86536940c3da865798363ce17d2616ea4bb4e
Dmitriy Mironenko <dim@uain.su>
2021-04-07 13:04:31 +0300

    changed gitignore / added new components

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View, StyleSheet} from "react-native";
+
+export const Navbar = props => {
+    const {title} = props;
+    return (
+        <View style={styles.navbar}>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    navbar: {
+        height: 70,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        backgroundColor: '#3949ab',
+        paddingBottom: 10,
+    },
+    text: {
+        color: 'white',
+        fontSize: 20,
+    }
+})
